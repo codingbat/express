ebad4010bd114921a6053149b8b60ba7bdcc6632
codingbat <Nazmul.Alam@students.ittralee.ie>
2017-02-28 23:10:02 +0000

    added mongo cloud service

--- server/config.js
+++ server/config.js
@@ -1,10 +1,14 @@
 (function () {
     module.exports = {
-        localdb:
-        {
+        localdb: {
             name: "naz-local-database",
             url: "mongodb://localhost/geo",
             port: 27017
+        },
+        cloud: {
+            name: "mongo-cloud",
+            url: "mongodb://nazmul:nazmul@cluster0-shard-00-00-rqcvv.mongodb.net:27017,cluster0-shard-00-01-rqcvv.mongodb.net:27017,cluster0-shard-00-02-rqcvv.mongodb.net:27017/geo?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin",
+            port: 27017,
         }
     };
 })();
